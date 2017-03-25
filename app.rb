e29885effebcb32de2391612efce05998dcf1d52
DivinoSilva <fernandinho_07@hotmail.com>
2017-03-25 15:22:47 -0300

    Retorno Json

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require 'rubygems'
 require 'sinatra'
 require 'httparty'
 
-get '/' do
-  erb :index, :locals => {result: nil}
+	get '/' do
+
+		if params['url']
+
+		escaped  = URI.escape(params['url'])
+		url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+		response = HTTParty.get(url)
+		parsed   = JSON.parse(response.body)['results']
+
+		end
+	erb :index, :locals => {result: parsed}
 end
+
